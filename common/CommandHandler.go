5b2a2e0734df01c5919e16882944adde00c53bda
brayden-arthur <braydenarthur@gmail.com>
2018-01-20 20:17:51 -0800

    update branch

--- common/CommandHandler.go
+++ common/CommandHandler.go
@@ -2,25 +2,24 @@ package common
 
 import (
 	"log"
+	"strconv"
 	"strings"
 )
 
-
-type CommandHandler struct{
-	commands map[int]func(args Command)
+type CommandHandler struct {
+	commands map[int]func(Command)
 }
 
 func (command *CommandHandler) On(command_name int, function_to_call func(args Command)) {
 	command.commands[command_name] = function_to_call
 }
 
-func (command *CommandHandler) parse(command string) {
-	command_hex := CommandHexReplacement{}
+func (command *CommandHandler) parse(commandStr string) {
 
-	log.Println("Received!:", string(command))
-	parsed = strings.Split(command, ",")
-	command_type = command_hex.toCommandHex(parsed[0])
+	log.Println("Received!:", string(commandStr))
+	parsed := strings.Split(commandStr, ",")
+	command_type, err := strconv.ParseInt(parsed[0], 10, 0)
 
-	defer command.commands[command_type](parsed)
+	defer command.commands[int(command_type)](parsed)
 
 }
---
+++ common/mapper.go
@@ -0,0 +1,57 @@
+package common
+
+type CommandHexReplacement int
+
+const (
+	ADD CommandHexReplacement = 1 + iota
+	QUOTE
+	BUY
+	COMMIT_BUY
+	CANCEL_BUY
+	SELL
+	COMMIT_SELL
+	CANCEL_SELL
+	SET_BUY_AMOUNT
+	CANCEL_SET_BUY
+	SET_BUY_TRIGGER
+	SET_SELL_AMOUNT
+	SET_SELL_TRIGGER
+	CANCEL_SET_SELL
+	DUMPLOG
+	ADMIN_DUMPLOG
+	DISPLAY_SUMMARY
+)
+
+var Commands = [...]string{
+	"ADD",
+	"QUOTE",
+	"BUY",
+	"COMMIT_BUY",
+	"CANCEL_BUY",
+	"SELL",
+	"COMMIT_SELL",
+	"CANCEL_SELL",
+	"SET_BUY_AMOUNT",
+	"CANCEL_SET_BUY",
+	"SET_BUY_TRIGGER",
+	"SET_SELL_AMOUNT",
+	"SET_SELL_TRIGGER",
+	"CANCEL_SET_SELL",
+	"DUMPLOG",
+	"ADMIN_DUMPLOG",
+	"DISPLAY_SUMMARY",
+}
+
+func (chr CommandHexReplacement) commandToString() string {
+	return Commands[chr-1]
+}
+
+func commandToInt(s string) int {
+	for k, v := range Commands {
+		if s == v {
+			return k
+		}
+	}
+	return -1
+}
+
