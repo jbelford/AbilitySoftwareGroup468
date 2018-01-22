d8239b1f7ea592d47f81d04186d464a4f8fda055
mattpaletta <mattpaletta@gmail.com>
2018-01-22 11:28:03 -0800

    Added workload generator

---
+++ workloadgenerator.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+    "bufio"
+    "log"
+    "os"
+    "strings"
+    "sync"
+    "strconv"
+    "net/http"
+    "encoding/json"
+    "bytes"
+    "time"
+)
+
+func main() {
+  if len(os.Args) < 3 {
+    panic("Missing arguments: <workLoadFile> <threadCount>")
+  }
+  pathToFile := os.Args[1]
+  threadCount, _ := strconv.ParseInt(os.Args[2], 10, 0)
+  file, err := os.Open(pathToFile)
+  if err != nil {
+      log.Fatal(err)
+  }
+  defer file.Close()
+
+  scanner := bufio.NewScanner(file)
+
+  var linesInFiles []string
+  for scanner.Scan() {
+      line := scanner.Text()
+      parsed := strings.Split(line, "] ")[1]
+      linesInFiles = append(linesInFiles, parsed)
+  }
+
+  sliceLength := len(linesInFiles)
+  //log.Println(linesInFiles)
+  log.Println("Sending:", sliceLength, "commands.")
+
+  if err := scanner.Err(); err != nil {
+      log.Fatal(err)
+  }
+
+  // Start up threads to hit server...
+  var wg sync.WaitGroup
+  wg.Add(int(threadCount))
+  sentMessages := make([]int, int(threadCount))
+
+  url := "https://localhost:8080"
+
+  log.Println("Sending Traffic using " + string(threadCount) +  " threads...")
+  start := time.Now()
+
+  for i := 0; i < int(threadCount); i++ {
+      go func(i int) {
+          defer wg.Done()
+          for j := 0; j < int(sliceLength); j++ {
+            line := linesInFiles[j]
+            values := map[string]string{"data": line}
+
+            jsonValue, _ := json.Marshal(values)
+
+            _, err := http.Post(url, "application/json", bytes.NewBuffer(jsonValue))
+
+            if err != nil {
+	               // handle error
+                 //log.Println(err)
+            }
+
+            sentMessages[i]++
+          }
+      }(i)
+  }
+
+  wg.Wait()
+  now := time.Now()
+  log.Println("Finished for loop")
+  log.Println("Ran for: ", now.Sub(start))
+  //log.Println(sentMessages)
+}
