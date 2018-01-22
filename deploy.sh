913faec45645eaaa1b49936c126e59c8ccd79cde
mattpaletta <mattpaletta@gmail.com>
2018-01-22 13:05:49 -0800

    Fixed docker deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 cd $(go env GOPATH)/src/github.com/mattpaletta/AbilitySoftwareGroup468 && git pull origin master
 go install github.com/mattpaletta/AbilitySoftwareGroup468
-docker build --file Dockerfile --tag ability:latest
+docker image build --file Dockerfile --tag ability:latest .
 
