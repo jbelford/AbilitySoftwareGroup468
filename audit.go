5437faac030703ed520c31df13e7a0cb72182382
jbelford <jackbelford12@live.ca>
2018-01-26 16:17:14 -0800

    Logging

--- audit.go
+++ audit.go
@@ -1,49 +1,19 @@
 package main
 
 import (
-	"encoding/xml"
 	"github.com/mattpaletta/AbilitySoftwareGroup468/logging"
 	"log"
 	"net"
 	"net/rpc"
-	"os"
 
 	"github.com/mattpaletta/AbilitySoftwareGroup468/common"
 )
 
 type AuditServer struct{}
 
-func log_msg(MSG string) {
-	log.Println("TODO:// Add user ID to log_msg struct")
-	user := "1"
-
-	user_log, err := xml.MarshalIndent(MSG, "  ", "    ")
-	if err != nil {
-		log.Println("error: %v\n", err)
-	}
-
-	f1, err := os.OpenFile(user+".txt", os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		panic(err)
-	}
-
-	f2, err := os.OpenFile("all_users.txt", os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		panic(err)
-	}
-
-	defer f1.Close()
-	defer f2.Close()
-
-	enc := xml.NewEncoder(f1)
-	enc.Indent("  ", "    ")
-	enc.Encode(user_log)
-	enc2 := xml.NewEncoder(f2)
-	enc2.Encode(user_log)
-}
-
 func (ad *AuditServer) Start() {
-	logger := new(logging.Logger)
+	logger, writer := logging.GetLoggerRPC()
+	defer writer.Close()
 	rpc.Register(logger)
 	ln, err := net.Listen("tcp", common.CFG.AuditServer.Url)
 	if err != nil {
--- logging/types.go
+++ logging/types.go
@@ -6,73 +6,72 @@ import (
 
 type Args struct {
 	Action, Command, Cryptokey, DebugMessage, ErrorMessage, FileName, Server, StockSymbol, Username string
-	QuoteServerTime, TransactionNum                                                                 int
-	Funds, Price                                                                                    int64
-	Timestamp                                                                                       uint64
+	Funds, Price, TransactionNum                                                                    int64
+	Timestamp, QuoteServerTime                                                                      uint64
 }
 
 type UserCommand struct {
-	XMLName        xml.Name `xml:"serCommand"`
+	XMLName        xml.Name `xml:"userCommand"`
 	Timestamp      uint64   `xml:"timestamp"`
-	TransactionNum string   `xml:"transactionNum"`
+	TransactionNum int64    `xml:"transactionNum"`
 	Command        string   `xml:"command"`
 	Username       string   `xml:"username, omitempty"`
 	StockSymbol    string   `xml:"stockSymbol, omitempty"`
 	Filename       string   `xml:"filename, omitempty"`
-	Funds          string   `xml:"funds, omitempty"`
+	Funds          int64    `xml:"funds, omitempty"`
 }
 
 type QuoteServer struct {
-	XMLName         xml.Name `xml:"serCommand"`
+	XMLName         xml.Name `xml:"quoteServer"`
 	Timestamp       uint64   `xml:"timestamp"`
-	TransactionNum  string   `xml:"transactionNum"`
-	Price           string   `xml:"price"`
+	TransactionNum  int64    `xml:"transactionNum"`
+	Price           int64    `xml:"price"`
 	StockSymbol     string   `xml:"stockSymbol"`
 	Username        string   `xml:"username"`
-	QuoteServerTime string   `xml:"quoteServerTime"`
+	QuoteServerTime uint64   `xml:"quoteServerTime"`
 	Cryptokey       string   `xml:"cryptokey"`
 }
 
 type AccountTransaction struct {
-	XMLName        xml.Name `xml:"serCommand"`
+	XMLName        xml.Name `xml:"accountTransaction"`
 	Timestamp      uint64   `xml:"timestamp"`
-	TransactionNum string   `xml:"transactionNum"`
+	TransactionNum int64    `xml:"transactionNum"`
 	Action         string   `xml:"action"`
 	Username       string   `xml:"username"`
-	Funds          string   `xml:"funds"`
+	Funds          int64    `xml:"funds"`
 }
 
 type SystemEvent struct {
-	XMLName        xml.Name `xml:"serCommand"`
+	XMLName        xml.Name `xml:"systemEvent"`
 	Timestamp      uint64   `xml:"timestamp"`
-	TransactionNum string   `xml:"transactionNum"`
+	TransactionNum int64    `xml:"transactionNum"`
 	Command        string   `xml:"command"`
 	Username       string   `xml:"username, omitempty"`
 	StockSymbol    string   `xml:"stockSymbol, omitempty"`
 	Filename       string   `xml:"filename, omitempty"`
-	Funds          string   `xml:"funds, omitempty"`
+	Funds          int64    `xml:"funds, omitempty"`
 }
 
 type ErrorEvent struct {
-	XMLName        xml.Name `xml:"serCommand"`
+	XMLName        xml.Name `xml:"errorEvent"`
 	Timestamp      uint64   `xml:"timestamp"`
-	TransactionNum string   `xml:"transactionNum"`
+	TransactionNum int64    `xml:"transactionNum"`
 	Command        string   `xml:"command"`
 	Username       string   `xml:"username, omitempty"`
 	StockSymbol    string   `xml:"stockSymbol, omitempty"`
 	Filename       string   `xml:"filename, omitempty"`
-	Funds          string   `xml:"funds, omitempty"`
+	Funds          int64    `xml:"funds, omitempty"`
 	ErrorMessage   string   `xml:"errorMessage, omitempty"`
 }
 
 type DebugEvent struct {
-	XMLName        xml.Name `xml:"serCommand"`
+	XMLName        xml.Name `xml:"debugEvent"`
 	Timestamp      uint64   `xml:"timestamp"`
-	TransactionNum string   `xml:"transactionNum"`
+	TransactionNum int64    `xml:"transactionNum"`
 	Command        string   `xml:"command"`
 	Username       string   `xml:"username, omitempty"`
 	StockSymbol    string   `xml:"stockSymbol, omitempty"`
 	Filename       string   `xml:"filename, omitempty"`
-	Funds          string   `xml:"funds, omitempty"`
+	Funds          int64    `xml:"funds, omitempty"`
 	DebugMessage   string   `xml:"debugMessage, omitempty"`
 }
