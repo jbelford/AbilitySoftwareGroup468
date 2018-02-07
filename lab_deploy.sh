bd4496e5157a1c2022cd1faef11fdd6b64d0aea1
mattpaletta <mattpaletta@gmail.com>
2018-02-06 18:59:37 -0800

    Updated lab deploy script

--- lab_deploy.sh
+++ lab_deploy.sh
@@ -1,12 +1,20 @@
-images = find . -name "*.docker"
+images=($(ls -d *.docker))
 
-for img in $images
+for img in "${images[@]}"
 do
-  echo "Loading: $img" && docker load < img && echo "Finished: $img"
+  dockimg=$(sed 's/\.docker/:latest/g' <<< $img)
+  echo "Loading: $img" && docker load < $img && echo "Loaded: $dockimg"
 done
 
+echo "Starting Registry Server" && docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
+
+for img in "${images[@]}"
+do
+  dockimg=$(sed 's/\.docker/:latest/g' <<< $img)
+  echo "Tagging Image: $dockimg" && docker tag $dockimg localhost:5000/$(sed 's/\.docker//g' <<< $img) \
+    && echo "Pushing: $img" && docker push localhost:5000/$(sed 's/\.docker//g' <<< $img)
+done
 
 echo "Deploying Stack" && docker stack deploy ability --compose-file docker-compose.yml
 echo "Current Services: " && docker service ls
-
